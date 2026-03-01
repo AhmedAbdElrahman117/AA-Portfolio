4af2f21c9c09de2827d943c6bae8752c703fd3a6
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2026-03-01 17:26:04 +0200

    fixing paths issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ahmedabdelrahman117.github.io',
+  base: '/AA-Portfolio/',
+
   // PERF: Minify HTML output — removes whitespace, comments, collapses attributes
   compressHTML: true,
 
